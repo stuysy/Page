b0252fc3d58b9be34da8484e0748b6851704dca5
stuysy <1152625751@qq.com>
2019-06-04 22:01:33 +0800

    5/28

---
+++ app/src/main/java/com/example/ysy/page/MyPageAdapter.java
@@ -0,0 +1,32 @@
+package com.example.ysy.page;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MyPageAdapter extends FragmentPagerAdapter {
+    private String [] title = new String[]{"大熊猫","考拉","北极熊"};
+    public MyPageAdapter(FragmentManager manager){
+        super(manager);
+    }
+    @Override
+    public Fragment getItem(int i) {
+        if(i==0){
+            return new FirstFragment();
+        }else if(i==1){
+            return new SecondFragment();
+        }else{
+            return new ThirdFragment();
+        }
+    }
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return title[position];
+    }
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
